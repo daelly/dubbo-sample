8435055c232c6923a2574cc593c73487fc2032cf
liqingyang <liqingyang@yy.com>
2023-10-23 11:02:54 +0800

    run with embedded zookeeper server

--- dubbo-provider/src/main/java/com/daelly/dubbo/provider/DubboProviderApplication.java
+++ dubbo-provider/src/main/java/com/daelly/dubbo/provider/DubboProviderApplication.java
@@ -1,13 +1,18 @@
 package com.daelly.dubbo.provider;
 
+import com.daelly.dubbo.provider.zookeeper.embedded.EmbeddedZookeeper;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
+@EnableDubbo
 @SpringBootApplication
 public class DubboProviderApplication {
 
     public static void main(String[] args) {
+        new EmbeddedZookeeper(2181, true).start();
         SpringApplication.run(DubboProviderApplication.class, args);
     }
 }
